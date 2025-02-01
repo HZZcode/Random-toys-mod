3d9922ac67b5b1e2ec7853888e9c899566ff1653
ZZ H <HZZcode@outlook.com>
2025-02-01 12:30:45 +0800

    now with ZZs in the gilded DIM

--- src/main/java/random_toys/zz_404/DisposableSpawnerBlockEntity.java
+++ src/main/java/random_toys/zz_404/DisposableSpawnerBlockEntity.java
@@ -4,6 +4,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.Spawner;
 import net.minecraft.entity.EntityType;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
@@ -27,5 +31,21 @@ public class DisposableSpawnerBlockEntity extends BlockEntity implements Spawner
 
     public void tick(World world, BlockPos pos, BlockState state) {
         DisposableSpawnerBlock.spawn(world, pos);
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        if (this.entityType != null) {
+            Identifier id = Registries.ENTITY_TYPE.getId(this.entityType);
+            nbt.putString("EntityType", id.toString());
+        }
+    }
+
+    @Override
+    protected void readNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        if (nbt.contains("EntityType")) {
+            Identifier id = Identifier.of(nbt.getString("EntityType"));
+            this.entityType = Registries.ENTITY_TYPE.get(id);
+        }
     }
 }
