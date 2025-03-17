fbfc65f97306c352a70e7ab52da24f06b1c0ecf9
ZZ H <HZZcode@outlook.com>
2025-03-17 19:42:30 +0800

    better fluid transformation mixin

--- src/main/java/random_toys/zz_404/mixin_utils/MixinSets.java
+++ src/main/java/random_toys/zz_404/mixin_utils/MixinSets.java
@@ -3,10 +3,8 @@ package random_toys.zz_404.mixin_utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.registry.tag.FluidTags;
 import random_toys.zz_404.reflection_utils.TrinketUtils;
 
 public class MixinSets {
@@ -27,21 +25,5 @@ public class MixinSets {
         EndermanAvoidStarringItems.add(Items.CARVED_PUMPKIN);
 
         BeaconBlockSpecialCaseBlocks.add(Blocks.BEDROCK);
-
-        FluidTransformationRules.add(FluidTransformationRule.create()
-                .fromFluid(fluid -> fluid.getDefaultState().isIn(FluidTags.LAVA))
-                .toFluid(FluidState::isStill)
-                .nearFluid(fluidState -> fluidState.isIn(FluidTags.WATER))
-                .transformTo(Blocks.OBSIDIAN));
-        FluidTransformationRules.add(FluidTransformationRule.create()
-                .fromFluid(fluid -> fluid.getDefaultState().isIn(FluidTags.LAVA))
-                .toFluid(fluidState -> !fluidState.isStill())
-                .nearFluid(fluidState -> fluidState.isIn(FluidTags.WATER))
-                .transformTo(Blocks.COBBLESTONE));
-        FluidTransformationRules.add(FluidTransformationRule.create()
-                .fromFluid(fluid -> fluid.getDefaultState().isIn(FluidTags.LAVA))
-                .nearBlock(state -> state.isOf(Blocks.BLUE_ICE))
-                .onBlock(state -> state.isOf(Blocks.SOUL_SOIL))
-                .transformTo(Blocks.BASALT));
     }
 }
--- src/main/java/random_toys/zz_404/mixin_utils/mixin/FluidTransformationMixin.java
+++ src/main/java/random_toys/zz_404/mixin_utils/mixin/FluidTransformationMixin.java
@@ -10,9 +10,10 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import random_toys.zz_404.RandomToys;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import random_toys.zz_404.mixin_utils.FluidTransformationRule;
 import random_toys.zz_404.mixin_utils.MixinSets;
 
@@ -24,12 +25,8 @@ public abstract class FluidTransformationMixin {
 
     @Shadow protected abstract void playExtinguishSound(WorldAccess world, BlockPos pos);
 
-    /**
-     * @author ZZ_404
-     * @reason Provide a public API for fluid transformation
-     */
-    @Overwrite
-    private boolean receiveNeighborFluids(World world, BlockPos pos, BlockState state) {
+    @Inject(at = @At("HEAD"), method = "receiveNeighborFluids", cancellable = true)
+    private void receiveNeighborFluids(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
         for (FluidTransformationRule rule : MixinSets.FluidTransformationRules) {
             if (rule.checkOriginalFluid(world.getFluidState(pos)) && rule.checkFluid(fluid)
                     && rule.checkBelowBlock(world.getBlockState(pos.down()))
@@ -40,11 +37,11 @@ public abstract class FluidTransformationMixin {
                             || rule.checkNearBlock(world.getBlockState(blockPos))) {
                         world.setBlockState(pos, rule.transformBlock.getDefaultState());
                         playExtinguishSound(world, pos);
-                        return false;
+                        cir.setReturnValue(false);
+                        cir.cancel();
                     }
                 }
             }
         }
-        return true;
     }
 }
